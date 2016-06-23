3869033058dbc255b9f6ce53152687e246c35e4e
Samuel Portz <samuel.portz@gmail.com>
2016-06-23 17:12:40 +0200

    UmbracoStartup Events registrieren

---
+++ bookmark-manager/Classes/umbracoStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+
+namespace bookmark_manager.Classes {
+    public class umbracoStartup : IApplicationEventHandler {
+
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext) {
+            //throw new NotImplementedException();
+
+            // Hier EventHandler registieren
+            //ContentTypeService.SavedContentType += createUserBookmarkNodeAfterRegister;
+
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext) {
+            //throw new NotImplementedException();
+
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext) {
+            //throw new NotImplementedException();
+
+        }
+
+        private void createUserBookmarkNodeAfterRegister() {
+
+        }
+    }
+}
